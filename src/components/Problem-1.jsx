d3b431afeeeb9a7d1bbe897980612b3bb017b1f6
Saiful Islam Meraz <42317343+meraz007@users.noreply.github.com>
2023-02-23 14:18:28 +0600

    solved problem 1

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,53 +1,157 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
 const Problem1 = () => {
+    const [show, setShow] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [name, setName] = useState("");
+    const [status, setStatus] = useState("");
+    const [completed, setCompleted] = useState([]);
+    const [isClicked, setIsClicked] = useState(false);
 
-    const [show, setShow] = useState('all');
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setTasks([...tasks, { name, status }]);
+        setName("");
+        setStatus("");
+    };
 
-    const handleClick = (val) =>{
-        setShow(val);
-    }
+    const handleFilter = (status) => {
+        setShow(status);
+        if (status == "completed" || status == "active") {
+            setCompleted(
+                tasks.filter((task) => {
+                return task.status === status;
+                })
+            );
+        }
+
+        else {
+            tasks.sort((a, b) => {
+                if (a.status === "active" && b.status !== "active") return -1;
+                    if (
+                        a.status === "completed" &&
+                        b.status !== "active" &&
+                        b.status !== "completed"
+                    )
+                        return -1;
+                return 1;
+            });
+
+            setCompleted(tasks);
+        }
+            setIsClicked(true);
+    };
+
+  
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+                <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+                
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form
+                        onSubmit={handleSubmit}
+                        className="row gy-2 gx-3 align-items-center mb-4"
+                    >
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                                type="text"
+                                placeholder="Name"
+                                value={name}
+                                onChange={(event) => {
+                                setName(event.target.value);
+                                setIsClicked(false);
+                                setShow("")
+                                }}
+                                className="form-control"
+                            />
                         </div>
+
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input
+                                type="text"
+                                placeholder="Status"
+                                value={status}
+                                onChange={(event) => {
+                                setStatus(event.target.value);
+                                setIsClicked(false);
+                                setShow("")
+                                }}
+                                className="form-control"
+                            />
                         </div>
+
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">
+                                Submit
+                            </button>
                         </div>
                     </form>
                 </div>
+
                 <div className="col-8">
                     <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button
+                                className={`nav-link ${show === "all" && "active"}`}
+                                type="button"
+                                onClick={() => handleFilter("all")}
+                            >
+                                All
+                            </button>
                         </li>
+
                         <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
+                            <button
+                                className={`nav-link ${show === "active" && "active"}`}
+                                type="button"
+                                onClick={() => handleFilter("active")}
+                                value="active"
+                            >
+                                Active
+                            </button>
                         </li>
+
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
+                            <button
+                                className={`nav-link ${show === "completed" && "active"}`}
+                                type="button"
+                                onClick={() => handleFilter("completed")}
+                                value="completed"
+                            >
+                                Completed
+                            </button>
                         </li>
                     </ul>
+
                     <div className="tab-content"></div>
+
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
+
                         <tbody>
-                        
+                            {!isClicked &&
+                                tasks.map((task, index) => (
+                                <tr key={index}>
+                                    <td>{task.name}</td>
+                                    <td>{task.status}</td>
+                                
+                                </tr>
+                                ))}
+                                
+                            {isClicked &&
+                                completed.map((task, index) => (
+                                <tr key={index}>
+                                    <td>{task.name}</td>
+                                    <td>{task.status}</td>
+                                </tr>
+                                ))}
                         </tbody>
                     </table>
                 </div>
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,400 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 const Problem2 = () => {
+    const [contacts, setContacts] = useState([]);
+    const [usContacts, setUsContacts] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch("https://contact.mediusware.com/api/contacts/")
+        .then((res) => res.json())
+        .then((data) => setContacts(data.results));
+    }, []);
+
+    useEffect(() => {
+        fetch("https://contact.mediusware.com/api/country-contacts/United States/")
+        .then((res) => res.json())
+        .then((data) => setUsContacts(data.results));
+    }, []);
+
+    const [checked, setChecked] = useState(false);
+
+    const handleChange = () => {
+        setChecked(!checked);
+    };
+
+    const handleData = (contact) => {
+        setData(contact);
+    };
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChanges = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleClear = () => {
+        setChecked(false);
+        setSearchTerm("");
+        console.log("Hello")
+    };
+
+    const filteredData = contacts.filter((contact) =>
+        contact.phone.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    
+    const filteredData1 = usContacts.filter((contact) =>
+        contact.phone.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
+                <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
                 <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <button
+                        className="btn btn-lg btn-outline-primary"
+                        data-bs-toggle="modal"
+                        data-bs-target="#allContacts"
+                        type="button"
+                        onClick={handleClear}
+                    >
+                        All Contacts
+                    </button>
+                    <div
+                        className="modal fade"
+                        id="allContacts"
+                        tabIndex="-1"
+                        aria-labelledby="exampleModalLabel"
+                        aria-hidden="true"
+                    >
+                    <div className="modal-dialog modal-lg">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                                    Modal A
+                                </h1>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                    onClick={() => setChecked(false)}
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="mb-3 d-flex justify-content-evenly">
+                                        <button
+                                            type="button"
+                                            className="btn btn-outline-primary"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#allContacts"
+                                        >
+                                            All Contacts
+                                        </button>
+
+                                        <button
+                                            type="button"
+                                            className="btn btn-outline-warning"
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#usContacts"
+                                            onClick={handleClear}
+                                        >
+                                            US Contacts
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="btn "
+                                            data-bs-dismiss="modal"
+                                            style={{
+                                            background: "white",
+                                            border: "1px solid #46139f",
+                                            color: "dark",
+                                            }}
+                                        >
+                                            Close
+                                        </button>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <div>
+                                            <input
+                                            type="checkbox"
+                                            checked={checked}
+                                            onChange={handleChange}
+                                            />{" "}
+                                            Only Even
+                                        </div>
+
+                                        <input
+                                            type="text"
+                                            placeholder="Search"
+                                            value={searchTerm}
+                                            onChange={handleChanges}
+                                            className="ms-5 border-1 px-2"
+                                        />
+                                    </div>
+
+                                    <div className="text-center mt-2">
+                                        {filteredData.map((contact) => (
+                                        <div key={contact.id}>
+                                            {checked && contact.id % 2 == 0 && (
+                                                <div
+                                                    style={{
+                                                        border: "1px solid gray",
+                                                        padding: "5px",
+                                                        cursor: "pointer",
+                                                    }}
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#contact"
+                                                    onClick={() => handleData(contact)}
+                                                >
+                                                    {
+                                                        <span className="d-flex justify-content-between">
+                                                        <span>{contact.id}.</span>
+                                                        <span>{contact.phone}</span>
+                                                        </span>
+                                                    }
+                                                </div>
+                                            )}
+                                            {!checked && (
+                                                <div
+                                                    style={{
+                                                        border: "1px solid gray",
+                                                        padding: "5px",
+                                                        cursor: "pointer",
+                                                    }}
+                                                    data-bs-toggle="modal"
+                                                    data-bs-target="#contact"
+                                                    onClick={() => handleData(contact)}
+                                                >
+                                                {
+                                                    <span className="d-flex justify-content-between">
+                                                    <span>{contact.id}.</span>
+                                                    <span>{contact.phone}</span>
+                                                    </span>
+                                                }
+                                                </div>
+                                            )}
+                                        </div>
+                                    ))}
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="modal fade"
+                    id="contact"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">
+                            Modal C
+                        </h5>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-toggle="modal"
+                            data-bs-target="#allContacts"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                        </div>
+                        <div className="modal-body">
+                        <form>
+                            <div className="mb-3">
+                            <h1 htmlFor="message-text" className="col-form-label">
+                                Id : {data.id}
+                            </h1>
+                            
+                            <h1 htmlFor="message-text" className="col-form-label">
+                                Phone : {data.phone}
+                            </h1>
+                            </div>
+                        </form> 
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <button
+                    className="btn btn-lg btn-outline-warning"
+                    type="button"
+                    data-bs-toggle="modal"
+                    data-bs-target="#usContacts"
+                    onClick={handleClear}
+                >
+                    US Contacts
+                </button>
+                <div
+                    className="modal fade"
+                    id="usContacts"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog modal-lg">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                        <h1 className="modal-title fs-5" id="exampleModalLabel">
+                            Modal B
+                        </h1>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                            onClick={handleClear}
+                        ></button>
+                        </div>
+                        <div className="modal-body">
+                        <form>
+                            <div className="mb-3 d-flex justify-content-evenly">
+                            <button
+                                type="button"
+                                className="btn btn-outline-primary"
+                                data-bs-toggle="modal"
+                                data-bs-target="#allContacts"
+                                onClick={handleClear}
+                            >
+                                All Contacts
+                            </button>
+
+                            <button
+                                type="button"
+                                className="btn btn-outline-warning"
+                                data-bs-toggle="modal"
+                                data-bs-target="#usContacts"
+                            >
+                                US Contacts
+                            </button>
+                            <button
+                                type="button"
+                                className="btn "
+                                data-bs-dismiss="modal"
+                                style={{
+                                background: "white",
+                                border: "1px solid #46139f",
+                                color: "dark",
+                                }}
+                            >
+                                Close
+                            </button>
+                            </div>
+                            <div className="d-flex justify-content-between">
+                            <div>
+                                <input
+                                type="checkbox"
+                                checked={checked}
+                                onChange={handleChange}
+                                />{" "}
+                                Only Even
+                            </div>
+
+                            <input
+                                type="text"
+                                placeholder="Search"
+                                value={searchTerm}
+                                onChange={handleChanges}
+                                className="ms-5 border-1 px-2"
+                            />
+                            </div>
+                            <div className="text-center mt-3">
+                            {filteredData1.map((contact) => (
+                                <div key={contact.id}>
+                                {checked && contact.id % 2 == 0 && (
+                                    <div
+                                    style={{
+                                        border: "1px solid gray",
+                                        padding: "5px",
+                                        cursor: "pointer",
+                                    }}
+                                    data-bs-toggle="modal"
+                                    data-bs-target="#usContact"
+                                    onClick={() => handleData(contact)}
+                                    >
+                                    {
+                                        <span className="d-flex justify-content-between">
+                                            <span>{contact.id}.</span>
+                                            <span>{contact.phone}</span>
+                                        </span>
+                                    }
+                                    </div>
+                                )}
+                                {!checked && (
+                                    <div
+                                        style={{
+                                            border: "1px solid gray",
+                                            padding: "5px",
+                                            cursor: "pointer",
+                                        }}
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#usContact"
+                                        onClick={() => handleData(contact)}
+                                    >
+                                    {
+                                        <span className="d-flex justify-content-between">
+                                            <span>{contact.id}.</span>
+                                            <span>{contact.phone}</span>
+                                        </span>
+                                    }
+                                    </div>
+                                )}
+                                </div>
+                            ))}
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
                 </div>
                 
+                <div
+                    className="modal fade"
+                    id="usContact"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">
+                            Modal C
+                        </h5>
+                        <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-toggle="modal"
+                            data-bs-target="#usContacts"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                        </div>
+                        <div className="modal-body">
+                        <form>
+                            <div className="mb-3">
+                            <h1 htmlFor="message-text" className="col-form-label">
+                                Id : {data.id}
+                            </h1>
+                            
+                            <h1 htmlFor="message-text" className="col-form-label">
+                                Phone : {data.phone}
+                            </h1>
+                            </div>
+                        </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
             </div>
         </div>
     );
